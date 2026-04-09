ec612cc9f07e5d66d1ca3a2e6e6326bc87e6e055
nalaura <142354611+nalaura7@users.noreply.github.com>
2026-04-09 15:49:05 -0300

    Add files via upload

---
+++ aula 2/exercicio01.cpp
@@ -0,0 +1,10 @@
+
+/*exercicio 01*/
+#include <iostream>
+
+int main()
+using namespace std;
+{
+    cout<<"Bem-vindo ao C++!"<< endl;
+    return 0;
+}
---
+++ aula 2/exercicio02.cpp
@@ -0,0 +1,10 @@
+
+/*exercicio 02*/
+#include <iostream>
+
+int main()
+using namespace std;
+{
+    cout<<"Ana Lauraaa"<< endl;
+    return 0;
+}
